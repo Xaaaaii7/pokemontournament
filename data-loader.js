77973d3eb13bccd827d7e84be56f7c368975b66f
Xaaaaii7 <103595698+Xaaaaii7@users.noreply.github.com>
2025-06-30 13:59:58 +0200

    Create data-loader.js

---
+++ data-loader.js
@@ -0,0 +1,13 @@
+// Función para cargar datos JSON
+async function cargarDatos() {
+  const response = await fetch('data.json');
+  return await response.json();
+}
+
+// Función para calcular K/C
+function calcularKC(pokemon) {
+  return (pokemon.battles > 0 ? (pokemon.kills / pokemon.battles).toFixed(2) : '0.00');
+}
+
+// Exporta funciones y datos necesarios
+export { cargarDatos, calcularKC };
