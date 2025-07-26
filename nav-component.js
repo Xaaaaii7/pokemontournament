e47a6f42cc7371dc1992b1b868b398f9f550a79b
Xaaaaii7 <103595698+Xaaaaii7@users.noreply.github.com>
2025-07-26 12:53:51 +0200

    Actualizar nav-component.js

--- nav-component.js
+++ nav-component.js
@@ -21,7 +21,7 @@ export function renderNav(currentPage = '') {
         { href: "combates.html", text: "Combats" },
         {
             text: "Tests",
-            subItems: [ { href: "clasificacion-stat.html", text: "Stats"}
+            subItems: [ { href: "clasifiacion-stat.html", text: "Stats"}
             ]
         }
 
